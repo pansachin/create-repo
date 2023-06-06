8343e7a0fc37445165e66d322870e4ea083a8749
Sachin Prasad <sachin.prasad@pantheon.io>
2023-06-06 13:46:36 +0530

    Create repo script

---
+++ org/createrepo.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/go-github/v52/github"
+)
+
+// Repo - Represents a github repository.
+type Repo struct {
+	Name           string
+	Description    string
+	DefaultBranch  string
+	AutoInitialize bool
+	Private        bool
+	GitIgnore      string
+	CommitMessage  string
+}
+
+func main() {
+	t := "ghu_UCu3xiwRqg2I1TkdoUd1aCQtmjlYMN2TRAVh"
+	ctx := context.Background()
+	c := github.NewTokenClient(ctx, t)
+
+	r := Repo{
+		Name:           "pantheon-repo" + fmt.Sprint(time.Now().Unix()),
+		Description:    "Pantheon generated repo",
+		DefaultBranch:  "main",
+		AutoInitialize: true,
+		Private:        false,
+	}
+
+	repo, _, err := c.Repositories.Create(ctx, "sachin-test-org-01", &github.Repository{
+		Name:         &r.Name,
+		Description:  &r.Description,
+		MasterBranch: &r.DefaultBranch,
+		AutoInit:     &r.AutoInitialize,
+		Private:      &r.Private,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(2 * time.Second)
+
+	fmt.Printf("Repo Name: %s\n", *repo.Name)
+	fmt.Printf("Repo Description: %s\n", *repo.Description)
+	fmt.Printf("Repo URL: %s\n", *repo.HTMLURL)
+
+}
---
+++ template/createrepo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/go-github/v52/github"
+)
+
+// Repo - Represents a github repository.
+type Repo struct {
+	Name           string
+	Description    string
+	DefaultBranch  string
+	AutoInitialize bool
+	Private        bool
+	GitIgnore      string
+	CommitMessage  string
+}
+
+func main() {
+	t := "ghu_UCu3xiwRqg2I1TkdoUd1aCQtmjlYMN2TRAVh"
+	ctx := context.Background()
+	c := github.NewTokenClient(ctx, t)
+
+	r := Repo{
+		Name:           "pantheon-repo" + fmt.Sprint(time.Now().Unix()),
+		Description:    "Pantheon generated repo",
+		DefaultBranch:  "main",
+		AutoInitialize: true,
+		Private:        false,
+	}
+
+	repo, _, err := c.Repositories.CreateFromTemplate(ctx, "pansachin", "test-template", &github.TemplateRepoRequest{
+		Name:        &r.Name,
+		Description: &r.Description,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(2 * time.Second)
+
+	fmt.Printf("Repo Name: %s\n", *repo.Name)
+	fmt.Printf("Repo Description: %s\n", *repo.Description)
+	fmt.Printf("Repo URL: %s\n", *repo.HTMLURL)
+
+}
